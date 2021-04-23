cbeb38dd492217337d639d3c2e387a713cf39e12
Runar Serigstad <runars@stacc.com>
2021-04-23 11:54:12 +0200

    Added a bit of documentation

--- client/config.go
+++ client/config.go
@@ -1,6 +1,6 @@
 package client
 
 type FlowRC struct {
-	Context   string `json:"context"`
-	Namespace string `json:"namespace"`
+	Context   string `json:"context"` // Kubernetes cluster context
+	Namespace string `json:"namespace"` // Kubernetes cluster namespace
 }
